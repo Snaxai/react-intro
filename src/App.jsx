62d73c6ff6900e3da21357558aedd8999c82ae25
Daniel <Daniel-mossestad@hotmail.com>
2021-03-18 18:51:29 +0100

    added image component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import Header from "./components/Header";
+import Image from "./components/Image";
 
 function App() {
   return (
     <div>
       <Header />
+      <Image
+        src="https://media.giphy.com/media/pa37AAGzKXoek/giphy.gif"
+        alt="Carlton from Fresh Prince in Bel Air is dancing"
+      />
     </div>
   );
 }
